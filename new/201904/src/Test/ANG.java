6ada1f3cb832f47af7c762444bf0f45a381baed6
madokast <578562554@qq.com>
2019-04-28 15:42:44 +0800

    icanfly

---
+++ new/201904/src/Test/ANG.java
@@ -0,0 +1,26 @@
+package Test;
+
+import java.util.Scanner;
+
+public class ANG {
+    public static void main(String[] args) {
+        double ang = 15.0;
+        double length = 0.1963;
+        double angn , lengthn ;
+        double angTol = 0.0 ;
+        String s ;
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            s = scanner.next();
+            try {
+                angn = Double.parseDouble(s);
+                angTol+=angn;
+                System.out.println(Math.round(angn/ang*length*10000)/10000.+" "+Math.round(angn*10000)/10000.);
+            }catch (Exception e)
+            {
+                System.out.println("angTol = " + angTol);
+                angTol = 0.0 ;
+            }
+        }
+    }
+}
