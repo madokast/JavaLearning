764a541d6ab6cc651b900fdd4be196ce3f0de3df
madokast <578562554@qq.com>
2019-09-20 10:36:32 +0800

    呜呜呜

---
+++ Others/spring4Master/chapter2_jdk8_0920/src/main/java/com.smart.domain/Info.java
@@ -0,0 +1,15 @@
+package com.smart.domain;
+
+import java.io.Serializable;
+
+public class Info implements Serializable {
+    private String info;
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
